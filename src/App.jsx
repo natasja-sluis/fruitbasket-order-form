ba381f315fb2ec67e34482d9c05cfa87d02ca4a1
natasja-sluis <natasjag92@gmail.com>
2024-06-06 09:57:01 +0200

    set up fruit counter

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,72 @@
-
 import './App.css'
 
 function App() {
 
-  return (
-    <>
-        <h1>Fruitmand bezorgservice</h1>
-    </>
-  )
+    return (
+        <>
+            <h1>Fruitmand bezorgservice</h1>
+            <div className="fruit-container">
+                <div className="fruit-row">
+                    <div className="fruit-name">
+                        <span>🍓</span>
+                        <span>Aardbeien</span>
+                    </div>
+                    <div className="counter">
+                        <button type="button">-</button>
+                        <span>0</span>
+                        <button type="button">+</button>
+                    </div>
+                </div>
+            </div>
+
+            <div className="fruit-container">
+                <div className="fruit-row">
+                    <div className="fruit-name">
+                        <span>🍌</span>
+                        <span>Bananen</span>
+                    </div>
+                    <div className="counter">
+                        <button type="button">-</button>
+                        <span>0</span>
+                        <button type="button">+</button>
+                    </div>
+                </div>
+            </div>
+
+            <div className="fruit-container">
+                <div className="fruit-row">
+                    <div className="fruit-name">
+                        <span>🍎</span>
+                        <span>Appels</span></div>
+                    <div className="counter">
+                        <button type="button">-</button>
+                        <span>0</span>
+                        <button type="button">+</button>
+                    </div>
+                </div>
+
+            </div>
+            <div className="fruit-container">
+                <div className="fruit-row">
+                    <div className="fruit-name">
+                        <span>🥝</span>
+                        <span>Kiwis</span>
+                    </div>
+                    <div className="counter">
+                        <button type="button">-</button>
+                        <span>0</span>
+                        <button type="button">+</button>
+                    </div>
+                </div>
+            </div>
+
+            <div className="reset-container">
+                <div className="reset-row">
+                    <button type="button" className="reset-button">reset</button>
+                </div>
+            </div>
+        </>
+    )
 }
 
 export default App
